2bff032267ce12ff45ed20e73c00ece20b152b7f
Bryan <bryanleesugue79@gmail.com>
2026-09-11 18:08:53 +0800

    Simplify router.php for PHP server

--- backend/router.php
+++ backend/router.php
@@ -1,13 +1,12 @@
 <?php
 
-$publicPath = __DIR__ . '/public';
-
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? ''
 );
 
-if ($uri !== '/' && file_exists($publicPath . $uri)) {
+// Serve static files directly if they exist in /public
+if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
     return false;
 }
 
-require_once $publicPath . '/index.php';
+require_once __DIR__ . '/public/index.php';
