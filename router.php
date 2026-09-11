fa761f1b32deed218e857efeaf3a6dbe8e1f82c8
Bryan <bryanleesugue79@gmail.com>
2026-09-11 17:41:44 +0800

    Fix publicPath in router.php to require public/index.php

--- router.php
+++ router.php
@@ -1,12 +1,13 @@
 <?php
 
+$publicPath = __DIR__ . '/public';
+
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? ''
 );
 
-// Serve static files directly if they exist in /public
-if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
+if ($uri !== '/' && file_exists($publicPath . $uri)) {
     return false;
 }
 
-require_once __DIR__ . '/public/index.php';
+require_once $publicPath . '/index.php';
